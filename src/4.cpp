ff7d64d965f2bc9dfb00cc8ae3aab9d97a6e4838
plackduriga183 <plackduriga183@gmail.com>
2025-03-08 05:16:40 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x = 0.5;
+    double y = 0.3;
+    double z = 0.7;
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+    cout << "The value of z is: " << z << endl;
+    return 0;
+}
