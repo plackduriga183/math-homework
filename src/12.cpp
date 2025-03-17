edd728b9f94fec67eb70efc69c6f0bc4ace891ad
plackduriga183 <plackduriga183@gmail.com>
2025-03-17 06:19:47 +0000

    Add 12.cpp

---
+++ src/12.cpp
@@ -0,0 +1,8 @@
+int main() {
+  int x = 5;
+  double y = 2.5;
+  std::cout << "The sum of x and y is: " << x + y << '\n';
+  std::cout << "The difference of x and y is: " << x - y << '\n';
+  std::cout << "The product of x and y is: " << x * y << '\n';
+  std::cout << "The quotient of x and y is: " << x / y << '\n';
+}
