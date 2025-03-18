afeaeec64a21ed895ec2211cafd9209abb5c40e9
plackduriga183 <plackduriga183@gmail.com>
2025-03-18 07:28:47 +0000

    Add 13.cpp

---
+++ src/13.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+  int n = rand(); //generate a random number
+  cout << "The generated number is: " << n << endl;
+  return 0;
+}
