f9d2efeef136e8040a87d2b924d7e1d4e945d8d6
plackduriga183 <plackduriga183@gmail.com>
2025-04-23 10:16:09 +0000

    Add 30.cpp

---
+++ src/30.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    int z = 2;
+    
+    cout << "The sum of " << x << " and " << y << " is " << (x + y) << endl;
+    cout << "The product of " << x << " and " << y << " is " << (x * y) << endl;
+    
+    return 0;
+}
